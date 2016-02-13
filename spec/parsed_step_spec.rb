3213afa780f2bc8746dc31129e7a35d982d6d0cb
Kylee Tilley <kyleetilley@gmail.com>
2016-02-13 12:31:04 -0600

    Adding ParsedStep spec

---
+++ spec/parsed_step_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Curb::ParsedStep do
+  let(:parsed_step) { Curb::ParsedStep.new(:given, 'The application is open') }
+
+  describe :parsed_step do
+    describe :type do
+        it :equals_given do
+            expect(parsed_step.type).to eq :given
+        end
+    end
+    
+    describe :phrase do
+        it :equals do
+            expect(parsed_step.phrase).to eq 'The application is open'
+        end
+    end
+  end
+end
