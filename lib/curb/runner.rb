dfac9a0ca2e289ea56ad92967dff75dd7d0e339b
Kylee Tilley <kyleetilley@gmail.com>
2016-02-13 18:24:23 -0600

    Adding failed tests and time taken for tests

--- lib/curb/runner.rb
+++ lib/curb/runner.rb
@@ -10,25 +10,45 @@ module Curb
     end
 
     def run
+      failed_tests = []
+
       @features.each do |feature|
         puts "Feature: #{feature.phrase}"
 
         feature.steps.each do |step|
           puts "#{step.type.capitalize}: #{step.phrase}"
-          handlers = @handlers.select do |handler|
+
+          handler = @handlers.find do |handler|
             handler.test(step.phrase)
           end
 
-          handler = handlers.empty? ? Proc.new {} : handlers.first
+          if handler.nil?
+            puts "\tNo handler defined. Skipping"
+            next
+          end
+
+          handler_start = Time.now
 
           begin
             handler.call(step.phrase)
           rescue => e
-            puts "\t Failed!"
-          else
-            puts "\tPassed!"
+            failed_tests << [feature, step, e]
           end
+
+          handler_end = Time.now
+
+          puts "\tTime elapsed #{(handler_end - handler_start)*1000} milliseconds"
         end
+      end
+
+      puts "\n"
+      puts "Failed Tests"
+
+      failed_tests.each.with_index do |test, i|
+        feature, step, ex = test
+        backtrace = ex.backtrace.join("\n\t\t")
+
+        puts "\t#{i}) #{feature.phrase} - #{step.phrase}\n\t\t#{ex.class}: #{ex.message}\n\t\t#{backtrace}"
       end
     end
 
